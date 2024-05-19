f13084bd3a8afa2749c4de36f9b7cb62ffdd2857
Jon Richards <jon@jonrichards.net>
2024-05-18 22:41:31 -0700

    Add image models

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
+import autogen
 from autogen import ConversableAgent
+from autogen.agentchat.contrib import img_utils
+from autogen.agentchat.contrib.capabilities import generate_images
+from autogen.agentchat.contrib.capabilities.generate_images import ImageGeneration
+from autogen.cache import Cache
+from autogen.oai import openai_utils
 from base.base_logger import logger
-from base.oai_config import gpt_config
+from base.oai_config import gpt_config, image_config
 from dotenv import load_dotenv
+from IPython.display import display
+from PIL.Image import Image
 
 # Load environment variables
 load_dotenv()
 
 # Example of using the ConversableAgent class
-agent = ConversableAgent("chatbot", llm_config=gpt_config, human_input_mode="NEVER")
-reply = agent.generate_reply(messages=[{"content": "Tell me a joke.", "role": "user"}])
+gpt_agent = ConversableAgent("chatbot", llm_config=gpt_config, human_input_mode="NEVER")
+reply = gpt_agent.generate_reply(
+    messages=[{"content": "Tell me a joke.", "role": "user"}]
+)
 logger.debug(reply)
 print(reply)
--- app/base/oai_config.py
+++ app/base/oai_config.py
@@ -8,15 +8,22 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# Load OpenAI model and API key info
-config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
-logger.debug(config_list)
+# Load settings from env
+CACHE_SEED = int(os.environ.get("CACHE_SEED", random.randint(1, 1000000)))
+logger.debug("CACHE_SEED: %s", CACHE_SEED)
+TEMPERATURE = int(os.environ.get("TEMPERATURE", "0"))
+logger.debug("TEMPERATURE: %s", TEMPERATURE)
+TIMEOUT = int(os.environ.get("TIMEOUT", "300"))
+logger.debug("TIMEOUT: %s", TIMEOUT)
 
-# Load configs for desired model
+# Log OpenAI model and API key info
+logger.debug(autogen.config_list_from_json("OAI_CONFIG_LIST"))
+
+# Load configs for desired GPT model
 GPT_MODEL = os.environ.get("GPT_MODEL", "gpt-3.5-turbo")
 if GPT_MODEL == "gpt-4-turbo":
     # Load OpenAI gpt4turbo model config from env, or file
-    config_list = autogen.config_list_from_json(
+    gpt_config_list = autogen.config_list_from_json(
         "OAI_CONFIG_LIST",
         file_location=".",
         filter_dict={
@@ -25,7 +32,7 @@ if GPT_MODEL == "gpt-4-turbo":
     )
 elif GPT_MODEL == "gpt-4o":
     # Load OpenAI gpt4o model config from env, or file
-    config_list = autogen.config_list_from_json(
+    gpt_config_list = autogen.config_list_from_json(
         "OAI_CONFIG_LIST",
         file_location=".",
         filter_dict={
@@ -34,7 +41,7 @@ elif GPT_MODEL == "gpt-4o":
     )
 else:
     # Load OpenAI gpt35turbo model config from env, or file
-    config_list = autogen.config_list_from_json(
+    gpt_config_list = autogen.config_list_from_json(
         "OAI_CONFIG_LIST",
         file_location=".",
         filter_dict={
@@ -42,25 +49,51 @@ else:
         },
     )
 
-# Log model config
-logger.debug(config_list)
-
-# Load LLM settings from env
-CACHE_SEED = int(os.environ.get("CACHE_SEED", random.randint(1, 1000000)))
-logger.debug("CACHE_SEED: %s", CACHE_SEED)
-
-TEMPERATURE = int(os.environ.get("TEMPERATURE", "0"))
-logger.debug("TEMPERATURE: %s", TEMPERATURE)
-
-TIMEOUT = int(os.environ.get("TIMEOUT", "300"))
-logger.debug("TIMEOUT: %s", TIMEOUT)
+# Log GPT model config
+logger.debug(gpt_config_list)
 
 # Set up GPT config
 gpt_config = {
     "timeout": TIMEOUT,
     "cache_seed": CACHE_SEED,
-    "config_list": config_list,
+    "config_list": gpt_config_list,
     "temperature": TEMPERATURE,
 }
 
+# Log GPT config
 logger.debug("GPT Config: %s", gpt_config)
+
+# Load configs for desired image model
+IMAGE_MODEL = os.environ.get("IMAGE_MODEL", "dall-e-2")
+if IMAGE_MODEL == "dall-e-3":
+    # Load OpenAI dall-e-3 model config from env, or file
+    image_config_list = autogen.config_list_from_json(
+        "OAI_CONFIG_LIST",
+        file_location=".",
+        filter_dict={
+            "model": ["dall-e-3"],
+        },
+    )
+else:
+    # Load OpenAI gpt35turbo model config from env, or file
+    image_config_list = autogen.config_list_from_json(
+        "OAI_CONFIG_LIST",
+        file_location=".",
+        filter_dict={
+            "model": ["dall-e-2"],
+        },
+    )
+
+# Log GPT model config
+logger.debug(image_config_list)
+
+# Set up GPT config
+image_config = {
+    "timeout": TIMEOUT,
+    "cache_seed": CACHE_SEED,
+    "config_list": image_config_list,
+    "temperature": TEMPERATURE,
+}
+
+# Log image config
+logger.debug("Image Config: %s", image_config)
